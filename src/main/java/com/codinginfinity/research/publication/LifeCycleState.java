97e3664fe4d5f8ed35d1db4ba79b5f4a0c685c1a
Gian Paolo Buffo <u14446619@tuks.co.za>
2016-03-31 11:49:02 +0200

    Created and documented LifeCycleState.java

---
+++ src/main/java/com/codinginfinity/research/publication/LifeCycleState.java
@@ -0,0 +1,11 @@
+package com.codinginfinity.research.publication;
+
+/**
+ * An abstract class indicating the state of a publication in its life cycle.
+ *
+ * @author Gian Paolo Buffo
+ */
+
+public abstract class LifeCycleState {
+
+}
