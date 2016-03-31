bce25210702c91fe985fb2ed90ec5cb850f341b1
Gian Paolo Buffo <u14446619@tuks.co.za>
2016-03-31 11:57:37 +0200

    Created and documented PublicationDetails.java

---
+++ src/main/java/com/codinginfinity/research/publication/PublicationDetails.java
@@ -0,0 +1,15 @@
+package com.codinginfinity.research.publication;
+
+import java.util.Date;
+
+/**
+ * A class representing the details of a publication, such as the authors, title and envisaged publication date
+ *
+ * @author Gian Paolo Buffo
+ */
+
+public class PublicationDetails {
+    private Person authors;
+    private String title;
+    private Date envisagedPublicationDate;
+}
