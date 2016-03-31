bd4a0003cd851a5f64ea1139b1167475a8112aac
Gian Paolo Buffo <u14446619@tuks.co.za>
2016-03-31 12:04:14 +0200

    Created and documented PublicationState.java

---
+++ src/main/java/com/codinginfinity/research/publication/PublicationState.java
@@ -0,0 +1,19 @@
+package com.codinginfinity.research.publication;
+
+import java.util.Date;
+
+/**
+ * A class representing the current state of a publication.
+ *
+ * @author Gian Paolo Buffo
+ */
+
+public class PublicationState {
+    private PublicationTarget publicationTarget;
+    private PublicationDetails publicationDetails;
+    private Date date;
+    private LifeCycleState lifeCycleState;
+    private String reason;
+    // todo add PublicationType. PublicationType related classes can be in their own folder, since there are so many
+    private PublicationType publicationType;
+}
