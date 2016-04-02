9589d9457df0372101c396ceb2a109b489d1eed4
u14310156 <u14310156@tuks.co.za>
2016-04-02 17:08:30 +0200

    Fixed the directory structure for my mock objects

---
+++ src/main/java/com/codinginfinity/research/publication/ModifyPublicationTypeResponse.java
@@ -0,0 +1,30 @@
+/** ModifyPublicationTypeResponse Class
+* @author Elizabeth Bode
+* @version 1.0
+* @since 2016-03-17
+*/
+
+package com.codinginfinity.publications;
+
+public class ModifyPublicationTypeResponse {
+    PublicationType newPubType;
+    
+    /**
+    * Class constructor
+    */
+    public ModifyPublicationTypeResponse(PublicationType pt)
+    {
+        newPubType = pt;
+    }
+    
+    /**
+    * Returns the modifiedPublicationType using the getter
+    * @param PublicationType
+    *                     The function will return an instance of the PublicationType
+    * @return The instance of PublicationType
+    */
+    /*public PublicationType sendModifiedPublicationType()
+    {
+        return modPubTypeReq.getModifiedPublicationType();
+    }*/
+}
