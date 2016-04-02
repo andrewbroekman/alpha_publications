ebf0354f3da6568ca21f4b66642fd7eb32089d5e
u14310156 <u14310156@tuks.co.za>
2016-04-02 17:10:52 +0200

    Fixed the directory structure for my unit tests

---
+++ src/test/java/com/codinginfinity/research/publication/NotActiveTest.java
@@ -0,0 +1,87 @@
+/** NotActiveTest Class
+* @author Elizabeth Bode
+* @version 1.0
+* @since 2016-03-23
+*/
+
+package com.codinginfinity.publications;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NotActiveTest {
+    private TestNotActive testNotActive;
+    
+    @Before
+    public void preTestInitialization() {
+        try
+        {
+            Date testDate = new Date("October", 2016, 15);
+            testNotActive = new TestNotActive(testDate, "testDeactReason");
+        }
+        catch (NullPointerException ex)
+        {
+            fail("Contsructor claimed null pointer encounter when it didn't.");
+        }
+    }
+    
+    @After
+    public void postTestCleanup() {
+    }
+
+    /**
+     * Test of getDeactivationReason method
+     */
+    @Test
+    public void testGetDeactivationReason() {
+        System.out.println("getDeactivationReason");
+        String expResult = "testDeactReason";
+        String result = testNotActive.getDeactivationReason();
+        assertEquals(expResult, result);
+    }
+    
+    public class TestNotActive extends PublicationTypeState{
+        String deactivationReason;
+
+        /**
+        * Class constructor
+        */
+        public TestNotActive()
+        {
+            super();
+            deactivationReason = "";
+        }
+
+        /**
+        * Class constructor
+        */
+        public TestNotActive(Date effDate, String reason)
+        {
+            super(effDate);
+            deactivationReason = reason;
+        }
+
+        /**
+        * Setter for the deactivationReason variable
+        * @param reason
+        *         The variable that will be used to change the value of the deactivationReason variable
+        */
+        /*public void setDeactivationReason(String reason)
+        {
+            deactivationReason = reason;
+        }*/
+
+        /**
+        * Getter for deactivationReason
+        * @param String
+        *           Returns the deactivation reason
+        * @return The deactivationReason as a string
+        */
+        public String getDeactivationReason()
+        {
+            return deactivationReason;
+        }
+    }
+}
