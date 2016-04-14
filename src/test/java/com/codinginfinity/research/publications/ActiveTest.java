51c47dbbd3b0d855d93f74609d849269f4cd1a77
Gian Paolo Buffo <u14446619@tuks.co.za>
2016-04-14 11:28:39 +0200

    Fixed package path in unit tests

--- src/test/java/com/codinginfinity/research/publications/ActiveTest.java
+++ src/test/java/com/codinginfinity/research/publications/ActiveTest.java
@@ -4,7 +4,7 @@
 * @since 2016-03-23
 */
 
-package com.codinginfinity.publications;
+package com.codinginfinity.research.publications;
 
 import java.util.List;
 import org.junit.After;
--- src/test/java/com/codinginfinity/research/publications/GetPublicationsForGroupTest.java
+++ src/test/java/com/codinginfinity/research/publications/GetPublicationsForGroupTest.java
@@ -1,4 +1,4 @@
-package com.codinginfinity.research.publication;
+package com.codinginfinity.research.publications;
 
 
 import org.junit.After;
